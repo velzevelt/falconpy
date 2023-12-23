adb409160bd404a74ec123f532c8d9e0e82c0f96
velzevelt <novaraptor13@gmail.com>
2023-12-23 22:28:37 +0300

    a

--- src/falconpy.py
+++ src/falconpy.py
@@ -1,8 +1,33 @@
+#!/usr/bin/python
+
 import cv2
 import pytesseract
+import argparse
 from key_listener import KeyListener
 from PIL import Image
-import argparse
+from pynput.keyboard import Key, Listener
+
+class KeyListener:
+    should_quit = False
+    destroy_on_quit = True
+    quit_key = 'q'
+    key = None
+    listener = None
+    
+    def handle_quit(self, key):
+        self.key = key
+        k_string = ''
+        try:
+            k_string = key.char
+        except Exception:
+            return True
+        
+        self.should_quit = k_string == self.quit_key
+        return not self.should_quit
+
+    def __init__(self):
+        self.listener = Listener(on_press=self.handle_quit)
+        self.listener.start()
 
 
 def process_video(video: cv2.VideoCapture, out_file):
